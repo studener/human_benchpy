b89349a2b10673ac205af2bd501a573bab854ebd
studener <philip.studener@gmx.de>
2025-04-21 12:27:48 +0200

    draft visual_memory.py

---
+++ visual_memory.py
@@ -0,0 +1,105 @@
+import pygame
+import time
+import numpy as np
+import pandas as pd
+
+pygame.init()
+
+screen = pygame.display.set_mode((720,720))
+move_sound = pygame.mixer.Sound('assets/move.mp3')
+win_sound = pygame.mixer.Sound('assets/win.mp3')
+font = pygame.font.Font(size=75)
+points_font = pygame.font.Font(size=1000)
+screen.fill("#57B7F3")
+start_text = font.render('CLICK TO PLAY', True, 'white')
+text_pos = start_text.get_rect(center = pygame.display.get_surface().get_rect().center)
+screen.blit(start_text, text_pos)
+pygame.display.flip()
+clock = pygame.time.Clock()
+
+# Variables
+done = False # Puts game loop on hold until mouse has been clicked required number of times
+rounds = [] # stores which buttons lit up
+displayed = [1] # copy of rounds, of which elements get removed to check if right square was clicked
+counter = -1 # keep track of how many times mouse has been clicked
+squares = {'one':[15,15,220,220], 'two':[250,15,220,220], 'three':[485,15,220,220], 
+           'four':[15,250,220,220], 'five':[250,250,220,220], 'six':[485,250,220,220], 
+           'seven':[15,485,220,220], 'eight':[250,485,220,220], 'nine':[485,485,220,220]}
+last_item = set(['a'])
+
+def clicked_square(x,y):
+    options = pd.DataFrame([['one', 'two', 'three'], ['four', 'five', 'six'], ['seven', 'eight', 'nine']])
+    axisvals = np.array([240,480,720])
+    return options.loc[list(y<axisvals), list(x<axisvals)].iloc[0,0]
+
+while True:
+    # Process player inputs.
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            raise SystemExit
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            clicked = clicked_square(*pygame.mouse.get_pos())
+            if counter == -1:
+                done = True
+            elif clicked in displayed:
+                counter -= 1
+                square = clicked
+                pygame.draw.rect(screen, 'white', squares[square],border_radius=20)
+                if counter == 0:
+                    win_sound.play()
+                else:
+                    move_sound.play()
+                pygame.display.flip()
+
+                if counter == 0:
+                    screen.fill("#4A97C7")
+                    for i in squares:
+                        pygame.draw.rect(screen, '#3478C6', squares[i],border_radius=20)
+            else:
+                # This is what happens when you lose
+                screen.fill("#FF0000")
+                points = points_font.render(f'{len(rounds)-1}', True, '#B50012')
+                points_pos = points.get_rect(center = pygame.display.get_surface().get_rect().center)
+                screen.blit(points, points_pos)
+                # screen.blit(font.render('THIS WINDOW WILL CLOSE IN 5 SECONDS', True, 'white'), (10, 400))
+                pygame.display.flip()
+                print(f'\nYou lost. Score: {len(rounds)-1}')
+                time.sleep(3)
+                pygame.quit()
+                raise SystemExit
+
+            if counter == 0:
+                done = True
+                time.sleep(0.8)
+
+        while done == True:
+            done = False
+
+            # draw screen and squares
+            screen.fill("#57B7F3")
+            pygame.display.flip()
+            for i in squares:
+                pygame.draw.rect(screen, '#3478C6', squares[i],border_radius=20)
+
+            pygame.display.flip()
+            time.sleep(0.5)
+
+            to_display = np.random.choice(list(squares.keys()), replace=False, size=3)
+            displayed = to_display.copy()
+            counter = len(to_display)
+            
+            for i in to_display:
+                pygame.draw.rect(screen, 'white', squares[i],border_radius=20)
+            
+            pygame.display.flip()
+            win_sound.play()
+            time.sleep(1.5)
+
+            screen.fill("#57B7F3")
+            for i in squares:
+                pygame.draw.rect(screen, '#3478C6', squares[i],border_radius=20)
+
+            pygame.display.flip()  # Refresh on-screen display
+
+    clock.tick(60)         # wait until next frame (at 60 FPS)
